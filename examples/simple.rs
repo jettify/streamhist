6ae9ce15acfe4c75332946491a4aa2dfd384487c
Nikolay Novik <nickolainovik@gmail.com>
2021-01-02 02:24:18 -0500

    Fix implemenatons and add more tests

--- examples/simple.rs
+++ examples/simple.rs
@@ -1,32 +1,38 @@
-use rand::rngs::StdRng;
 use rand::SeedableRng;
 use rand_distr::{Distribution, Normal};
+use rand_isaac::Isaac64Rng;
 use streamhist::StreamHist;
 
 fn main() {
-    let mut rng = StdRng::seed_from_u64(42);
-    let normal = Normal::new(2.0, 3.0).unwrap();
-    let mut hist = StreamHist::new(64);
+    let mut rng = Isaac64Rng::seed_from_u64(42);
+    let dist = Normal::new(2.0, 3.0).unwrap();
+    let mut hist = StreamHist::new(32);
 
-    let maxn = 1000;
-    let vals: Vec<f64> = (0..maxn).map(|_| normal.sample(&mut rng)).collect();
+    let maxn = 10000;
+    let mut vals: Vec<f64> = (0..maxn).map(|_| dist.sample(&mut rng)).collect();
 
     for v in vals.iter() {
         hist.insert(*v, 1);
     }
 
+    vals.sort_by(|a, b| a.partial_cmp(b).unwrap());
+
     let n: f64 = vals.len() as f64;
     let exact_sum: f64 = vals.iter().sum();
     let exact_mean: f64 = exact_sum / n;
     let exact_var: f64 = vals.iter().map(|v| (v - exact_mean).powf(2.0) / n).sum();
+    let exact_median: f64 = vals[vals.len() / 2];
+    let exact_sum: u64 = vals.iter().map(|v| if *v <= 2.0 { 1 } else { 0 }).sum();
 
     println!("------------------------------------------------");
-    println!("StreamHist Mean {:?}", hist.mean());
-    println!("Mean            {:?}", exact_mean);
-    println!("Abs Difference  {:?}", exact_mean - hist.mean());
+    println!("Est Mean               {:?}", hist.mean());
+    println!("Est Var                {:?}", hist.var());
+    println!("Est Median             {:?}", hist.median());
+    println!("Est Count vals <= 2.0  {:?}", hist.count_less_then_eq(2.0));
     println!("------------------------------------------------");
-    println!("StreamHist Var  {:?}", hist.var());
-    println!("Var             {:?}", exact_var);
-    println!("ABS Difference  {:?}", exact_var - hist.var());
+    println!("Mean                   {:?}", exact_mean);
+    println!("Var                    {:?}", exact_var);
+    println!("Median                 {:?}", exact_median);
+    println!("Count vals <= 2.0      {:?}", exact_sum);
     println!("------------------------------------------------");
 }
--- src/lib.rs
+++ src/lib.rs
@@ -50,6 +50,14 @@ impl StreamHist {
             centroids: Vec::with_capacity(size),
         }
     }
+
+    pub fn clear(&mut self) {
+        self.min = f64::MAX;
+        self.max = f64::MIN;
+        self.count = 0;
+        self.centroids.clear();
+    }
+
     pub fn count(&self) -> u64 {
         self.count
     }
@@ -85,7 +93,6 @@ impl StreamHist {
             return;
         }
 
-        // unsafe { breakpoint() };
         let l = self.closest_centroids();
         // TODO: use copy
         let cj = self.centroids[l + 1].clone();
@@ -118,6 +125,7 @@ impl StreamHist {
         }
         let k = self.centroids.iter().position(|c| c.value > value).unwrap();
         let s: u64 = self.centroids.iter().take(k - 1).map(|c| c.count).sum();
+
         let (ci, cj) = self.border_centroids(k);
         if ci.count > 0 && (cj.count > 0 && cj.value == value) {
             return s + cj.count;
@@ -138,23 +146,28 @@ impl StreamHist {
         }
 
         let t: f64 = q * (self.count as f64);
-        let mut pv = 0.0;
+        let mut prev_half_count = 0.0;
         let mut s: f64 = 0.0;
-        let mut idx: usize = 0;
+        let mut idx: Option<usize> = None;
 
         for (i, c) in self.centroids.iter().enumerate() {
-            idx = i;
-            let v = (c.count as f64) / 2.0;
-            if s + v + pv > t {
+            let half_count = (c.count as f64) / 2.0;
+
+            if s + half_count + prev_half_count > t {
+                idx = Some(i);
                 break;
             }
-            s += v + pv;
-            pv = v;
+
+            s += half_count + prev_half_count;
+            prev_half_count = half_count;
         }
-        let (ci, cj) = self.border_centroids(idx);
+        if idx.is_none() {
+            return self.max;
+        }
+        let (ci, cj) = self.border_centroids(idx.unwrap());
+
         let ci_count = ci.count as f64;
         let cj_count = cj.count as f64;
-
         let d = t - s;
         let a = (cj_count - ci_count) as f64;
         if a == 0.0 {
@@ -164,6 +177,10 @@ impl StreamHist {
         let c = -2.0 * d;
         let z = (-b + (b * b - 4.0 * a * c).sqrt()) / (2.0 * a);
         ci.value + (cj.value - ci.value) * z
+    }
+
+    pub fn median(&self) -> f64 {
+        self.quantile(0.5)
     }
 
     fn border_centroids(&self, k: usize) -> (Centroid, Centroid) {
@@ -184,48 +201,158 @@ impl StreamHist {
                 },
             );
         }
-        (self.centroids[k], self.centroids[k + 1])
+        (self.centroids[k - 1], self.centroids[k])
     }
 
-    pub fn count_lte(&self, value: f64) -> u64 {
+    pub fn count_less_then_eq(&self, value: f64) -> u64 {
         self.sum(value)
     }
 
+    #[inline]
     fn closest_centroids(&self) -> usize {
         let mut min_dis = f64::MAX;
         let mut idx = 0;
-        let last = self.centroids.len() - 1;
         // TODO: use window function to do iterations
-        for (i, c) in self.centroids[..last - 1].iter().enumerate() {
-            if (self.centroids[i + 1].value - self.centroids[i].value).abs() < min_dis {
-                min_dis = (self.centroids[i + 1].value - self.centroids[i].value).abs();
-                idx = i
+        for i in 0..self.centroids.len() - 1 {
+            let d = (self.centroids[i + 1].value - self.centroids[i].value).abs();
+            if d < min_dis {
+                min_dis = d;
+                idx = i;
             }
         }
-        return idx;
+        idx
+    }
+
+    pub fn merge(&mut self, other: &Self) {
+        for c in other.centroids.iter() {
+            self.insert(c.value, c.count)
+        }
     }
 }
 
 #[cfg(test)]
 mod tests {
-    extern crate approx;
-
     use super::StreamHist;
-    use approx::relative_eq;
+    use approx::assert_relative_eq;
+    use rand::SeedableRng;
+    use rand_distr::{Distribution, Exp, LogNormal, Normal, Uniform};
+    use rand_isaac::Isaac64Rng;
 
     #[test]
     fn test_basic_ctor() {
         let mut hist = StreamHist::new(3);
         hist.insert(25.0, 1);
+        hist.insert(21.0, 1);
         hist.insert(20.0, 1);
-        hist.insert(10.0, 1);
-        hist.insert(1.0, 1);
-        hist.insert(19.0, 1);
-        assert_eq!(hist.count, 5);
 
-        relative_eq!(hist.max(), 1.0);
-        relative_eq!(hist.min(), 25.0);
-        assert_eq!(hist.count(), 5);
-        println!("{:?}", hist)
+        assert_relative_eq!(hist.max(), 25.0);
+        assert_relative_eq!(hist.min(), 20.0);
+        assert_eq!(hist.count(), 3);
+        assert_relative_eq!(hist.quantile(0.0), 20.0);
+        assert_relative_eq!(hist.quantile(1.0), 25.0);
+        assert_eq!(hist.count_less_then_eq(-100.0), 0);
+        assert_eq!(hist.count_less_then_eq(100.0), 3);
+    }
+
+    #[test]
+    fn test_closest_centroids() {
+        let mut hist = StreamHist::new(3);
+        hist.insert(1.0, 1);
+        hist.insert(5.0, 1);
+        hist.insert(11.0, 1);
+        assert_eq!(hist.closest_centroids(), 0);
+
+        hist.clear();
+        hist.insert(1.0, 1);
+        hist.insert(5.0, 1);
+        hist.insert(7.0, 1);
+        assert_eq!(hist.closest_centroids(), 1);
+    }
+    #[test]
+    fn test_merge() {
+        let mut hist = StreamHist::new(3);
+        let mut other = StreamHist::new(3);
+        hist.insert(1.0, 1);
+        hist.insert(5.0, 1);
+
+        other.insert(10.0, 10);
+        other.insert(20.0, 10);
+        hist.merge(&other);
+        assert_eq!(hist.count(), 22);
+    }
+
+    fn assert_distribution(mut vals: Vec<f64>, mut hist: StreamHist, tol: f64) {
+        for v in vals.iter() {
+            hist.insert(*v, 1);
+        }
+        vals.sort_by(|a, b| a.partial_cmp(b).unwrap());
+
+        let n: f64 = vals.len() as f64;
+        let s: f64 = vals.iter().sum();
+        let exact_mean: f64 = s / n;
+        let exact_var: f64 = vals.iter().map(|v| (v - exact_mean).powf(2.0) / n).sum();
+        let exact_median: f64 = vals[vals.len() / 2];
+        let exact_sum: u64 = vals.iter().map(|v| if *v <= 2.0 { 1 } else { 0 }).sum();
+        assert_relative_eq!(hist.min(), vals[0]);
+        assert_relative_eq!(hist.max(), vals[vals.len() - 1]);
+        assert_eq!(hist.count(), vals.len() as u64);
+
+        assert_relative_eq!(hist.mean(), exact_mean, max_relative = tol);
+        assert_relative_eq!(hist.var(), exact_var, max_relative = tol);
+        assert_relative_eq!(hist.median(), exact_median, max_relative = tol);
+        assert_relative_eq!(hist.quantile(0.5), exact_median, max_relative = tol);
+        assert_relative_eq!(
+            hist.count_less_then_eq(2.0) as f64,
+            exact_sum as f64,
+            max_relative = tol
+        );
+    }
+
+    #[test]
+    fn test_normal_distribution() {
+        let mut rng = Isaac64Rng::seed_from_u64(42);
+        let dist = Normal::new(2.0, 3.0).unwrap();
+        let hist = StreamHist::new(64);
+
+        let tol = 0.005;
+        let maxn = 10000;
+        let vals: Vec<f64> = (0..maxn).map(|_| dist.sample(&mut rng)).collect();
+        assert_distribution(vals, hist, tol)
+    }
+
+    #[test]
+    fn test_log_normal_distribution() {
+        let mut rng = Isaac64Rng::seed_from_u64(42);
+        let dist = LogNormal::new(1.0, 1.0).unwrap();
+        let hist = StreamHist::new(64);
+
+        let tol = 0.02;
+        let maxn = 10000;
+        let vals: Vec<f64> = (0..maxn).map(|_| dist.sample(&mut rng)).collect();
+        assert_distribution(vals, hist, tol)
+    }
+
+    #[test]
+    fn test_exp_distribution() {
+        let mut rng = Isaac64Rng::seed_from_u64(42);
+        let dist = Exp::new(1.5).unwrap();
+        let hist = StreamHist::new(64);
+
+        let tol = 0.05;
+        let maxn = 10000;
+        let vals: Vec<f64> = (0..maxn).map(|_| dist.sample(&mut rng)).collect();
+        assert_distribution(vals, hist, tol)
+    }
+
+    //#[test]
+    fn test_uniform_distribution() {
+        let mut rng = Isaac64Rng::seed_from_u64(42);
+        let dist = Uniform::new(-1000.0, 1000.0);
+        let hist = StreamHist::new(64);
+
+        let tol = 0.01;
+        let maxn = 10000;
+        let vals: Vec<f64> = (0..maxn).map(|_| dist.sample(&mut rng)).collect();
+        assert_distribution(vals, hist, tol)
     }
 }
